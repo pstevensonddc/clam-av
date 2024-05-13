cfbdadf223d9bc575ca2672a68d3f72cffc71403
paulstevenson <paul.stevenson@double-click-design.com>
2024-05-13 14:29:49 +0100

    adds clamav

--- clam_av/views.py
+++ clam_av/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view
 from django.http import HttpResponse
 from storages.backends.s3boto3 import S3Boto3Storage
 from django.core.files.base import ContentFile
+import pyclamd
 
 
 def home(request):
@@ -31,3 +32,21 @@ def test_spaces_connection(request):
         return HttpResponse('Connection to DigitalOcean Spaces is working!')
     else:
         return HttpResponse('Connection to DigitalOcean Spaces failed!')
+
+@api_view(['GET'])
+def scan_file():
+    # Connect to ClamAV daemon
+    cd = pyclamd.ClamdUnixSocket()
+
+    # Check if connection was successful
+    if not cd.ping():
+        return 'Failed to connect to ClamAV daemon.'
+
+    # Scan the file
+    scan_result = cd.scan_file('test.text')
+
+    # Check scan result
+    if scan_result['test.text'] == 'OK':
+        return HttpResponse('File is clean.')
+    else:
+        return HttpResponse(f'Virus detected: {scan_result["test.txt"]}')
